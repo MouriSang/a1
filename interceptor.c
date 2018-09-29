bc61d6dc5bca46ce9a56c577fef44dce28b08a31
Jason <haosun.zhou@mail.utoronto.ca>
2018-09-28 22:01:41 -0400

    fix

--- interceptor.c
+++ interceptor.c
@@ -1224,94 +1224,19 @@ static int init_function(void)
 
 
 
-/**
-
- * Module exits. 
-
- *
-
- * TODO: Make sure to:  
-
- * - Restore MY_CUSTOM_SYSCALL to the original syscall.
-
- * - Restore __NR_exit_group to its original syscall.
-
- * - Make sure to set the system call table to writable when making changes, 
-
- *   then set it back to read only once done.
-
- * - Make sure to deintercept all syscalls, and cleanup all pid lists.
-
- * - Ensure synchronization, if needed.
-
- */
-
-static void exit_function(void)
-
-{
-
-    
-
-    int y = 0; //Variable decleration at the beginning prevents compiler warning.
-
-    
-
-    //My Table
-
-    
-
-    spin_lock(&my_table_lock);
-
-    
-
-    while (y < NR_syscalls) {
-
-        if (table[y].intercepted == 1) {
-
-            spin_unlock(&my_table_lock);
-
-            my_syscall(REQUEST_SYSCALL_RELEASE, y, y);
-
-            spin_lock(&my_table_lock);
-
-        }
-
-        y++;
-
-    }
-
-    
-
-    spin_unlock(&my_table_lock);
-
-    
-
-    //Syscall table
-
-    
-
-    spin_lock(&sys_call_table_lock);
-
-    
-
-    set_addr_rw((unsigned long) sys_call_table); //Allow the syscall table to become readable and writable.
-
-    
-
-    sys_call_table[MY_CUSTOM_SYSCALL] = orig_custom_syscall;
-
-    sys_call_table[__NR_exit_group] = orig_exit_group;
-
-    
-
-    set_addr_ro((unsigned long) sys_call_table); //Set the syscall table to read-only.
-
-    
-
-    spin_unlock(&sys_call_table_lock);
-
-
-
+static void exit_function(void) {        
+	int sys_call_position = 0;
+	spin_lock(&sys_call_table_lock);
+	set_addr_rw((unsigned long)sys_call_table);
+	sys_call_table[MY_CUSTOM_SYSCALL] = orig_custom_syscall;
+	sys_call_table[__NR_exit_group] = orig_exit_group;
+	set_addr_ro((unsigned long)sys_call_table);
+	spin_unlock(&sys_call_table_lock);
+	spin_lock(&my_table_lock);
+	while (sys_call_position < NR_syscalls) {
+		destroy_list(sys_call_position);
+	}
+	spin_unlock(&my_table_lock);
 }
 
 
